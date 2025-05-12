f11140ff47685f623c6effe074a698c394fdd117
shenxin <wb_shenxin@meituan.com>
2025-05-12 23:55:19 +0800

    inject script

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,10 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
+  matches: ["<all_urls>"],
+  async main() {
+    console.log("Injecting script...");
+    await injectScript("/injected.js", {
+      keepInDom: true,
+    });
+    console.log("Done!");
   },
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,15 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
+  runner: {
+    disabled: true,
+  },
+  manifest: {
+    web_accessible_resources: [
+      {
+        resources: ["injected.js"],
+        matches: ["<all_urls>"],
+      },
+    ],
+  },
 });
